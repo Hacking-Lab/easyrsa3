f78b8814d7b0054a5f484e3b4b5bb85dbb40d4c5
Ivan Buetler <ivan.buetler@compass-security.com>
2021-05-17 09:13:30 +0200

    adding covenant cert

--- setup.sh
+++ setup.sh
@@ -12,3 +12,19 @@ CERT_COMMONNAME="*.${FQDN}"
 ./easyrsa --batch gen-dh
 
 
+# create covenant cert
+FQDN="hacking-lab.local"
+CERT_FILENAME="covenant.${FQDN}"
+CERT_COMMONNAME="covenant.${FQDN}"
+./easyrsa --batch --req-cn="${CERT_COMMONNAME}" gen-req ${CERT_FILENAME} nopass
+./easyrsa --batch sign-req server ${CERT_FILENAME}
+
+
+# create full cain.pem
+cat ./pki/ca.crt > ./pki/issued/covenant-fullchain.crt
+cat ./pki/issued/covenant.hacking-lab.local.crt >> ./pki/issued/covenant-fullchain.crt
+
+# create pfx for covenant
+openssl pkcs12 -export -in ./pki/issued/covenant-fullchain.crt -inkey ./pki/private/covenant.hacking-lab.local.key -out ./pki/issued/covenant-fullchain.pfx -name covenant.hacking-lab.local -passout pass:CovenantDev
+
+
