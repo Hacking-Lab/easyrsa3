bba84a7c9331267ba72eb6ce2a099859243de0d3
Ivan Buetler <ivan.buetler@compass-security.com>
2021-05-17 07:31:50 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# setup PKI
+./easyrsa --batch init-pki
+./easyrsa --batch build-ca nopass
+
+# create wildcard cert
+FQDN="hacking-lab.local"
+CERT_FILENAME="wildcard.${FQDN}"
+CERT_COMMONNAME="*.${FQDN}"
+./easyrsa --batch --req-cn="${CERT_COMMONNAME}" gen-req ${CERT_FILENAME} nopass
+./easyrsa --batch sign-req server ${CERT_FILENAME}
+./easyrsa --batch gen-dh
+
+
